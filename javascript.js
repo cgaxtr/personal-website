0381cfbb6d33100d005976788ad1619838d08435
Carlos Gómez <cga.xtr@gmail.com>
2018-02-17 14:49:56 +0100

    initial commit

---
+++ javascript.js
@@ -0,0 +1,8 @@
+$('#link').click(function(e){				
+		e.preventDefault();
+		var strAncla=$(this).attr('href');
+			$('body,html').stop(true,true).animate({				
+				scrollTop: $(strAncla).offset().top
+			},1000);
+		
+	});
